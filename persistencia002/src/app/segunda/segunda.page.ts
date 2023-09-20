66a794d508ee468f00768d21e13f4e364e950cc9
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-09-20 09:42:30 -0300

    segunda

--- persistencia002/src/app/segunda/segunda.page.ts
+++ persistencia002/src/app/segunda/segunda.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-segunda',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./segunda.page.scss'],
 })
 export class SegundaPage implements OnInit {
-
-  constructor() { }
+  valorParametro : any;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.valorParametro = this.route.snapshot.paramMap.get("numerito");
   }
 
 }

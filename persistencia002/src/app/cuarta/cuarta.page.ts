612a14e55ea22cf9382391d65f70505cbf241f91
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-09-28 08:46:09 -0300

    update

--- persistencia002/src/app/cuarta/cuarta.page.ts
+++ persistencia002/src/app/cuarta/cuarta.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Storage } from '@ionic/storage';
-
+// instalar: npm install @ionic/storage-angular
+// en app.module import { IonicStorageModule } from '@ionic/storage-angular'; 
 
 @Component({
   selector: 'app-cuarta',
@@ -19,6 +20,6 @@ export class CuartaPage implements OnInit {
   async agregar()
   {
     //await this.storage.set("1","hola");
-    await this.storage.set("1","hola");
+    await this.storage.set(this.id,this.nombre);
   }
 }

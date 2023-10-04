9f3f9f1374ee1fb1d2db33e22e16c40ba6f4db22
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2023-10-04 11:09:57 -0300

    update

--- persistencia002/src/app/quinta/quinta.page.ts
+++ persistencia002/src/app/quinta/quinta.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CrudService } from './crud.service';
+import { ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-quinta',
@@ -9,16 +11,67 @@ import { CrudService } from './crud.service';
 export class QuintaPage implements OnInit {
   //rut:any;
   persona:any =[];
-  constructor(private crud: CrudService) { }
+  constructor(private crud: CrudService,
+    private toast: ToastController) { }
 
   ngOnInit() {
   }
 
-  agregar()
+  async agregar()
   {
-    console.log("hola");
-    console.log(this.persona);
+    // Ejercicio 1: Validar todos los datos  antes de guardar
+    // usar TOAST rojo error y verde guardado.
+    //console.log("hola");
+    //console.log(this.persona);
     //this.persona.rut = this.rut;
-    this.crud.guardar(this.persona.rut, this.persona);
+    if(!this.persona.rut)
+      this.mensajeError("Falta rut");
+    else if(!this.persona.nombre)
+      this.mensajeError("Falta Nombre");
+    else if(!this.persona.direccion)
+      this.mensajeError("Falta Dirección");
+    else if(!this.persona.correo)
+      this.mensajeError("Falta Correo");
+    else if(!this.persona.telefono)
+      this.mensajeError("Falta Teléfono");
+      else if(!this.persona.anioNacimiento)
+        this.mensajeError("Falta Año de Nacimiento");
+    else
+    {
+      this.crud.guardar(this.persona.rut, this.persona);
+      this.persona = [];
+      const t = await this.toast.create({
+        message     : 'Guardado con éxito',
+        icon        : 'checkmark-circle-outline',
+        duration    : 3000,
+        color       : 'success',
+        position    : 'middle'
+      });
+      t.present();
+    }
+  }
+  
+// Ejercicio 2: Terminar el crud para los datos de persona
+
+  async mensajeError(mensaje: string)
+  {
+    const t = await this.toast.create({
+      message     : mensaje,
+      icon        : 'alert-outline',
+      duration    : 3000,
+      color       : 'danger' 
+    });
+    t.present();
+  }
+  async buscar()
+  {
+    if(!this.persona.rut)
+      this.mensajeError("Falta rut");
+    else
+    {
+      console.log(this.persona.rut);
+      this.crud.leer(this.persona.rut).then(x => {this.persona = x;});
+      console.log(this.persona);
+    }
   }
 }
